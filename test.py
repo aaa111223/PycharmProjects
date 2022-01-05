65462a35521b8eab29d1e2fc7b513b1cbce0ca94
aaa111223 <30249604@163.com>
2022-01-05 20:59:40 +0800

    更新test

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 #coding=utf-8
 #测试文件
 
+from _typeshed import Self
 import random
-import selenium
+
+class test(Self,object):
+    def main(self,object):
+        print("hello world")
+        
+        
+        
